6972b861b1544da049b06b5966afc1fce058a059
Jasmine Moreira <jasmine.moreira.2013@gmail.com>
2021-07-30 22:29:30 -0300

    Update titanic.py

--- pytorch/titanic.py
+++ pytorch/titanic.py
@@ -33,6 +33,8 @@ test_labels = t.as_tensor(test_set.Survived.values)
 train_set =  train_set.iloc[: , :-2]
 test_set =  test_set.iloc[: , :-2]
 
+n_feat = len(train_set[1])
+
 train_set = t.tensor(train_set.values, dtype=t.float32)
 test_set = t.tensor(test_set.values, dtype=t.float32)
 
@@ -40,9 +42,9 @@ use_cuda = t.cuda.is_available()
 device = t.device("cuda:0" if use_cuda else "cpu") 
 
 # Criar Modelo
-net = nn.Sequential(nn.Linear(7, 50),
+net = nn.Sequential(nn.Linear(n_feat, 20),
                     nn.ReLU(),
-                    nn.Linear(50, 2)
+                    nn.Linear(20, 2)
                    )
 
 if use_cuda:
@@ -52,15 +54,14 @@ if use_cuda:
 criterion = nn.CrossEntropyLoss()
 optimizer = t.optim.AdamW(net.parameters(), lr=0.01) #e-1
 
-
 # loop de treinamento
-epoch = 10
+epoch = 20
 loss_values = []
 for epoch in range(epoch):
     epoch_loss = 0
     for x,y in zip(train_set, train_labels):    
         optimizer.zero_grad()
-        output = net(x.view(1,7).to(device)).to(device)
+        output = net(x.view(1,n_feat).to(device)).to(device)
         loss = criterion(output, y.view(1).to(device))
         loss.backward()
         optimizer.step()
@@ -80,7 +81,7 @@ with t.no_grad():
     correct = 0
     total = 0
     for x,y in zip(train_set, train_labels):      
-        output = net(x.view(1,7).to(device)).to(device)
+        output = net(x.view(1,n_feat).to(device)).to(device)
         if t.argmax(output) == y:
             correct +=1
         total +=1
