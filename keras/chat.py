1928bb01f226704226a81c302d23397e1b401a1e
Jasmine Moreira <jasmine.moreira.2013@gmail.com>
2021-08-13 20:43:50 -0300

    Update chat.py

--- keras/chat.py
+++ keras/chat.py
@@ -13,6 +13,7 @@ import numpy as np
 import string
 from spellchecker import SpellChecker
 from tensorflow.keras import optimizers
+from tensorflow.keras import regularizers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Embedding, Dropout
 from tensorflow.keras.utils import to_categorical
@@ -42,11 +43,11 @@ def spck(sentences):
         checked_q.append(q)
     return checked_q
 
-#Converter perguntas para minúsculas    
+#Converter perguntas para minúsculas e corrigir a sintaxe    
 qdf.question = spck(qdf.question)
 
-max_len = 50
-max_words = 1000
+max_len = 50        # tamanho máximo da frase
+max_words = 1000    # tamanho do dicionário
 
 #Tokenizar e codificar as perguntas
 tokenizer = Tokenizer(num_words=max_words)
@@ -66,6 +67,47 @@ model.add(Dense(100))
 model.add(Dense(adf.answer_id.max()+1, activation = 'softmax'))
 
 opt = optimizers.Adam(learning_rate=0.0001)
-model.compile(optimizer=opt, loss='binary_crossentropy',metrics=['acc'])
+model.compile(optimizer=opt, loss='binary_crossentropy',metrics=['accuracy'])
 
 history = model.fit(x_train, y_train, epochs=2000, batch_size=len(x_train))
+
+
+# Plotar gráficos de perda e acurácia
+
+import matplotlib.pyplot as plt
+
+history_dict = history.history
+loss_values = history_dict['loss']
+#val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len( history_dict['loss']) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training Loss')
+#plt.plot(epochs, val_loss_values, 'b', label='Validation Loss')
+plt.title("Training and Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+#plt.clf()
+acc_values = history_dict['accuracy']
+#val_acc_values = history_dict['val_accuracy']
+
+plt.plot(epochs, acc_values, 'bo', label='Training Acc')
+#plt.plot(epochs, val_acc_values, 'b', label='Validation Acc')
+plt.title("Training and Validation Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
